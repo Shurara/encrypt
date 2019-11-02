e2c4980d603a0576f6ce0a51eea4f46406e0f8b2
Shurara <o.rodiuk@gmail.com>
2019-11-02 22:30:46 +0200

    update  readFromFile method, scanner closed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+package encryptdecrypt;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -117,6 +117,7 @@ public class Main {
             FileReader reader = new FileReader(inputFileName);
             Scanner sc = new Scanner(reader);
             textFromFile = sc.nextLine();
+            sc.close();
             return textFromFile;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -138,53 +139,3 @@ public class Main {
     }
 
 }
-
-/*
-package encryptdecrypt;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String mode = "enc";
-        int key = 0;
-        String data = "";
-
-        for (int i = 0; i < args.length; i += 2) {
-            if (args[i].equals("-mode")) {
-                mode = args[i+1];
-
-            } else if (args[i].equals("-key")) {
-                key = Integer.parseInt(args[i+1]);
-
-            } else if (args[i].equals("-data")) {
-                data = args[i+1];
-            }
-        }
-
-        switch (mode) {
-            case "enc":
-                getEncryption(data, key);
-                break;
-            case "dec":
-                getDecryption(data, key);
-                break;
-            default:
-                System.out.println("Unknown operation");
-                break;
-        }
-    }
-
-    public static void getDecryption(String data, int key) {
-        for (char item : data.toCharArray()) {
-            char shiftItem = (char) (item - key);
-            System.out.print(shiftItem);
-        }
-    }
-
-    public static void getEncryption(String data, int key) {
-        for (char item : data.toCharArray()) {
-            char shiftItem = (char) (item + key);
-            System.out.print(shiftItem);
-        }
-    }
-}*/
